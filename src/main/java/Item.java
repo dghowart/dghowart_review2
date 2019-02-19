7110a8dcfae62c21f08f3c968b9079d2669b4d4e
ProgKing <dhowarth161@gmail.com>
2019-02-19 14:55:30 -0700

    fixed bug-08 (added getters and setters) for Item.java

--- src/main/java/Item.java
+++ src/main/java/Item.java
@@ -1,4 +1,9 @@
 package main.java;
+/*File: Item.java
+ * Author: amehlhas, adalvi1
+ * Date: 2/19/19
+ * Description: Item class that is used as the "elements" of a set
+ * */
 
 public final class Item {
         public int key;
@@ -7,4 +12,10 @@ public final class Item {
                 key = keyValue;
         }
         // further components and methods
+        public int getKey() {
+        	return key;
+        }
+        public void setKey(int key) {
+        	this.key = key;
+        }
 }
